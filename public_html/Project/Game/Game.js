7ba92af2d93bf5f831ed720d0926c9172d653258
leyki reynoso <leykijrreynoso@gmail.com>
2022-07-11 12:28:51 -0400

    game created

--- public_html/Project/Game/Game.js
+++ public_html/Project/Game/Game.js
@@ -3,7 +3,7 @@ const context = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
-
+const scoreEL = document.querySelector('#scoreEl')
 class Player {
     constructor(x,y,radius,color) {
         this.x = x
@@ -61,6 +61,12 @@ class Enemy{
     }
 }
 
+function gameOver(){
+    context.fillStyle = '#000000';
+    context.font = '24px Arial';
+    context.textAlign = 'center';
+    context.fillText('Game Over. Final Score: ' + score, canvas.width / 2, canvas.height/4);
+}
 
 const player = new Player(canvas.width/2, canvas.height/2,30,'red')
 player.draw()
@@ -69,29 +75,61 @@ const enemies = []
 
 function spawnEnemies(){
     setInterval(() => {
-        const radius = 30
-        const x = Math.random() * canvas.width
-        const y = Math.random() * canvas.height
+        const radius = 20
+        
+        let x
+        let y
+
+        if(Math.random() < 0.5){
+            x = Math.random() < 0.5 ? 0 - radius: canvas.width + radius
+            y = Math.random() * canvas.height
+        }
+        else{
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas, height + radius
+        }
         const color = 'green'
-        const angle = Math.atan2(player.y - y, palter.x - x)
+        const angle = Math.atan2(player.y - y, player.x - x)
         const velocity = {x: Math.cos(angle), y: Math.sin(angle)}
         enemies.push(new Enemy(x, y, radius, color, velocity))
     }, 1000)
 }
+let animationId
+let score = 0
 
 function animate()
-{
-    requestAnimationFrame(animate)
+{ 
+    animationId = requestAnimationFrame(animate)
     context.clearRect(0,0,canvas.width,canvas.height)
     player.draw()
-    projectiles.forEach((projectile) =>{ projectile.update()})
-    enemies.forEach((enemy) => {
+    projectiles.forEach((projectile) =>{projectile.update()})
+    
+    enemies.forEach((enemy, index) => {
         enemy.update()
-    })
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+            if(dist - enemy.radius - player.radius < 1)
+            {
+                cancelAnimationFrame(animationId)
+                gameOver()
+            }
+        projectiles.forEach((projectile, projectileIndex) => {
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+            if(dist - enemy.radius - projectile.radius < 1)
+                {
+                score += 1
+                scoreEL.innerHTML = score
+
+                setTimeout(() => {
+                    enemies.splice(index, 1)
+                    projectiles.splice(projectileIndex, 1)
+                }, 0);
+             }
+            })
+        })
 }
 
 addEventListener('click', (event) => {
-    const angle = Math.atan2(event.clientY - player.x, event.clientX - player.y)
+    const angle = Math.atan2(event.clientY - player.y, event.clientX - player.x)
     const velocity = {x: Math.cos(angle), y: Math.sin(angle)}
     projectiles.push(new Projectile(player.x,player.y, 5, 'orange', velocity))
 })
