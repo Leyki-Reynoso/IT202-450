3f07045eca1c03266b5e22cb7964d93f155b5404
leyki reynoso <leykijrreynoso@gmail.com>
2022-07-14 20:25:44 -0400

    scores can now be saved

--- public_html/Project/save_scores.php
+++ public_html/Project/save_scores.php
@@ -1,17 +1,15 @@
 <?php
+    require(__DIR__ . "/../../lib/functions.php");
+    session_start();
     //TODO save scores
-    $score = $_REQUEST['q'];
-    if (is_logged_in(true)) {
+    $score = $_POST['text'];
+    if (isset($_SESSION["user"])) {
         $db = getDB();
         $params = [":user_id" => get_user_id(), ":score" => $score];
         $stmt = $db->prepare("INSERT INTO Scores (user_id, score) VALUES(:user_id, :score)");
-        try {
-            $stmt->execute($params);
-            flash("Successfully registered!", "success");
-        } catch (Exception $e) {
-            users_check_duplicate($e->errorInfo);
-        }
+        $stmt->execute($params);
+        flash("Successfully registered!", "success");
+
         error_log("Session data: " . var_export($_SESSION, true));
     }
-    
 ?>
