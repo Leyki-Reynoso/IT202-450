199ec787b909f2f82dcf758eb69fb8940020179d
leyki reynoso <leykijrreynoso@gmail.com>
2022-07-16 13:12:55 -0400

    all functionalities implemented(only styling left)

---
+++ public_html/Project/Game_menu.php
@@ -0,0 +1,17 @@
+<?php 
+    require_once(__DIR__ . "/../../partials/nav.php");
+?>
+
+<html>
+<body>
+
+<h2>Menu</h2>
+
+<p>shoot with your mouse to kill the enemies that come from outside the screen</p>
+
+<li><a href="<?php echo get_url('Game.html'); ?>">play</a></li>
+<li><a href="<?php echo get_url('personal_scores.php'); ?>">scores</a></li>
+
+
+</body>
+</html>
